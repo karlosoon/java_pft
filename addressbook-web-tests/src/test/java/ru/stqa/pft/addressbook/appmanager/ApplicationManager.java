53e26950bc2b02a6507af77cd59f233f9cea1c85
karlosoon <maximka1103@gmail.com>
2020-10-19 14:48:20 +0300

    Добавил if else для выбора драйвера браузера (Chrome, IE, Firefox)

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
@@ -2,7 +2,10 @@ package ru.stqa.pft.addressbook.appmanager;
 
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.remote.BrowserType;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -14,10 +17,21 @@ public class ApplicationManager {
     private  GroupHelper groupHelper;
     JavascriptExecutor js;
     public Map<String, Object> vars;
+    private String browser;
+
+    public ApplicationManager(String browser) {
+        this.browser = browser;
+    }
 
     public void init() {
         System.setProperty("webdriver.gecko.driver","C:\\gecko\\geckodriver.exe");
-        driver = new FirefoxDriver();
+        if (browser == BrowserType.FIREFOX) {
+            driver = new FirefoxDriver();
+        } else if (browser == BrowserType.CHROME) {
+            driver = new ChromeDriver();
+        } else if (browser == BrowserType.IE) {
+            driver = new InternetExplorerDriver();
+        }
         js = (JavascriptExecutor) driver;
         vars = new HashMap<String, Object>();
         groupHelper = new GroupHelper(driver);
