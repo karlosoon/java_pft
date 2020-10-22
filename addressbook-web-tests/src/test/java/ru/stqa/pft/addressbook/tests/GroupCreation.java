2bdad6f2a6181dfc846784c5fae3f18633158b68
karlosoon <maximka1103@gmail.com>
2020-10-22 12:13:21 +0300

    В тестах на группы добавил List<> для подсчета кол-ва групп до и после изменений

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreation.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreation.java
@@ -3,16 +3,19 @@ import org.junit.Assert;
 import org.junit.Test;
 import ru.stqa.pft.addressbook.model.GroupData;
 
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class GroupCreation extends TestBase {
 
   @Test
   public void testGroupCreation() {
     app.getNavigationHelper().goToGroups();
-    int before = app.getGroupHelper().getGroupCount();
+    List<GroupData> before = app.getGroupHelper().getGroupList();
     app.getGroupHelper().createGroup(new GroupData("test1", "test2", "test3"));
-    int after = app.getGroupHelper().getGroupCount();
-    Assert.assertEquals(after, before + 1);
+    List<GroupData> after = app.getGroupHelper().getGroupList();
+    Assert.assertEquals(after.size(), before.size() + 1);
   }
 
 }
