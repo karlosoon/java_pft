357c331f67465a394cb426745a5056feea776f77
karlosoon <maximka1103@gmail.com>
2020-10-22 14:49:31 +0300

    Добавил сравнение множеств в GroupModificationTest

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/GroupHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/GroupHelper.java
@@ -23,8 +23,11 @@ public class GroupHelper extends HelperBase {
     }
 
     public void fillGroupFields(GroupData groupData) {
+        driver.findElement(By.name("group_name")).clear();
         type(By.name("group_name"), groupData.getName());
+        driver.findElement(By.name("group_header")).clear();
         type(By.name("group_header"), groupData.getHeader());
+        driver.findElement(By.name("group_footer")).clear();
         type(By.name("group_footer"), groupData.getFooter());
     }
 
@@ -68,7 +71,8 @@ public class GroupHelper extends HelperBase {
         List<WebElement> elements = driver.findElements(By.cssSelector("span.group"));
         for (WebElement element : elements){
             String name = element.getText();
-            GroupData group = new GroupData(name, null, null);
+            String id = element.findElement(By.tagName("input")).getAttribute("value");
+            GroupData group = new GroupData(id, name, null, null);
             groups.add(group);
         }
         return groups;
--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTest.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTest.java
@@ -4,6 +4,7 @@ import org.junit.Assert;
 import org.junit.Test;
 import ru.stqa.pft.addressbook.model.GroupData;
 
+import java.util.HashSet;
 import java.util.List;
 
 public class GroupModificationTest extends TestBase {
@@ -12,15 +13,20 @@ public class GroupModificationTest extends TestBase {
     public void testGroupModification() {
         app.getNavigationHelper().goToGroups();
         if (! app.getGroupHelper().isThereAGroup()){
-            app.getGroupHelper().createGroup(new GroupData("test1", "test2", "test3"));
+            app.getGroupHelper().createGroup(new GroupData("test1", null, null));
         }
         List<GroupData> before = app.getGroupHelper().getGroupList();
         app.getGroupHelper().selectGroup(before.size() - 1);
         app.getGroupHelper().initGroupModification();
-        app.getGroupHelper().fillGroupFields(new GroupData("test1", "test2", "test3"));
+        GroupData group = new GroupData(before.get(before.size() - 1).getId(), "test1", "test2", "test3");
+        app.getGroupHelper().fillGroupFields(group);
         app.getGroupHelper().submitGroupModification();
         app.getGroupHelper().returnToGroupPage();
         List<GroupData> after = app.getGroupHelper().getGroupList();
         Assert.assertEquals(after.size(), before.size());
+
+        before.remove(before.size() - 1);
+        before.add(group);
+        Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
     }
 }
