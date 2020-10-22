44d202e49c03ad685d0b655cfc58629e875a56aa
karlosoon <maximka1103@gmail.com>
2020-10-22 15:21:42 +0300

    Добавил сравнение групп после создания новой группы

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreation.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreation.java
@@ -4,6 +4,7 @@ import org.junit.Test;
 import ru.stqa.pft.addressbook.model.GroupData;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 
 
@@ -13,9 +14,20 @@ public class GroupCreation extends TestBase {
   public void testGroupCreation() {
     app.getNavigationHelper().goToGroups();
     List<GroupData> before = app.getGroupHelper().getGroupList();
-    app.getGroupHelper().createGroup(new GroupData("test1", "test2", "test3"));
+    GroupData group = new GroupData("test1", "test2", "test3");
+    app.getGroupHelper().createGroup(group);
     List<GroupData> after = app.getGroupHelper().getGroupList();
     Assert.assertEquals(after.size(), before.size() + 1);
+
+    before.add(group);
+    int max = 0;
+    for (GroupData g : after){
+      if (g.getId() > max){
+        max = g.getId();
+      }
+    }
+    group.setId(max);
+    Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
   }
 
 }
